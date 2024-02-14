b6390a02eebacacf40567542bb4319a93c7f5b20
Darren <darrenchang951@gmail.com>
2024-02-14 17:51:55 +0800

    Ignore neo-tree for lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,16 +1,19 @@
 return {
-  {
-    -- https://github.com/nvim-lualine/lualine.nvim
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "dracula",
-        },
-      })
-    end,
-  }
+	{
+		-- https://github.com/nvim-lualine/lualine.nvim
+		"nvim-lualine/lualine.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			require("lualine").setup({
+				options = {
+					theme = "dracula",
+					globalstatus = false,
+					disabled_filetypes = { "neo-tree" },
+					ignore_focus = { "neo-tree", "lazy" },
+				},
+			})
+		end,
+	},
 }
