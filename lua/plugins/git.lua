304a5eb008cdb302abaf11128266a6b453ba914e
Darren <darrenchang951@gmail.com>
2024-02-14 17:09:03 +0800

    Added gitsigns

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -9,4 +9,23 @@ return {
       require("lazygit")
     end,
   },
+  {
+    -- https://github.com/lewis6991/gitsigns.nvim
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      local gitsignsconfig = require("gitsigns")
+      gitsignsconfig.setup({
+        word_diff  = true,
+        current_line_blame = true,
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'right_align', -- 'eol' | 'overlay' | 'right_align'
+          delay = 0,
+          ignore_whitespace = false,
+          virt_text_priority = 100,
+        },
+        current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      })
+    end
+  }
 }
