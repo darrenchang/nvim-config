843d31d7fc1a8a3e97ce8ea099e95a35ad28e515
Darren <darrenchang951@gmail.com>
2024-02-14 16:45:24 +0800

    Refactor

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,14 +1,16 @@
--- https://github.com/nvim-lualine/lualine.nvim
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("lualine").setup({
-      options = {
-        theme = "dracula",
-      },
-    })
-  end,
+  {
+    -- https://github.com/nvim-lualine/lualine.nvim
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "dracula",
+        },
+      })
+    end,
+  }
 }
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,16 @@
--- https://github.com/nvim-neo-tree/neo-tree.nvim
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-    "3rd/image.nvim",
+  {
+    -- https://github.com/nvim-neo-tree/neo-tree.nvim
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      "3rd/image.nvim",
+    },
+    config = function()
+      vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
+    end,
   },
-  config = function()
-    vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
-  end,
 }
