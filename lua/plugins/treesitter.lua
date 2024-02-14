f6e6b231a93b5cadfa71b6d175e396292f479187
Darren <darrenchang951@gmail.com>
2024-02-14 17:09:22 +0800

    Refactor

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,16 @@
--- https://github.com/nvim-treesitter/nvim-treesitter
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = { "lua", "javascript", "python" },
-      auto_instal = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+  {
+    -- https://github.com/nvim-treesitter/nvim-treesitter
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = { "lua", "javascript", "python" },
+        auto_instal = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  }
 }
