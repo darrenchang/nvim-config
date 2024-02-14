1bcd5aac27133cf8577ffb6f8e19afb387bfbb9a
Darren <darrenchang951@gmail.com>
2024-02-14 17:09:15 +0800

    Renamed colorscheme file

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    -- https://github.com/catppuccin/nvim
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  }
+}
