63ac6e7d37e09ee938583452dde063bd425e2c1a
Darren <darrenchang951@gmail.com>
2024-02-14 18:03:47 +0800

    Follow file for neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -14,6 +14,11 @@ return {
       require("neo-tree").setup({
         filesystem = {
           use_libuv_file_watcher = true,
+          follow_current_file = {
+            enabled = true,
+            leave_dirs_open = false,
+          },
+
         },
       })
     end,
