b658f1c1fd731698bb54982e27a925aa69cd0a2e
Darren <darrenchang951@gmail.com>
2024-02-16 00:17:24 +0800

    Added git explorer

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,13 +10,22 @@ return {
       "3rd/image.nvim",
     },
     config = function()
-      vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
+      vim.keymap.set("n", "<leader>e", ":Neotree toggle<CR>", {})
       require("neo-tree").setup({
         filesystem = {
           enable_refresh_on_write = true,
           follow_current_file = {
             enabled = true,
             leave_dirs_open = true,
+          },
+        },
+        sources = { "filesystem", "git_status" },
+        source_selector = {
+          winbar = true,
+          content_layout = "center",
+          sources = {
+            { source = "filesystem", display_name = "File" },
+            { source = "git_status", display_name = "Git" },
           },
         },
       })
