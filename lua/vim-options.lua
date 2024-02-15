3efb231d4c18a751bd512793c4bdabd2f0437bb6
Darren <darrenchang951@gmail.com>
2024-02-16 01:09:14 +0800

    Hide command line unless needed

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber number")
+vim.cmd("set cmdheight=0")
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
