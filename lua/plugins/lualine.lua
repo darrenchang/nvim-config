c3da3712c08bc0aa55ed61aba8af53bd2c212934
Darren <darrenchang951@gmail.com>
2024-02-16 19:21:12 +0800

    Show search counts in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,19 +1,25 @@
 return {
-	{
-		-- https://github.com/nvim-lualine/lualine.nvim
-		"nvim-lualine/lualine.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("lualine").setup({
-				options = {
-					theme = "dracula",
-					globalstatus = false,
-					disabled_filetypes = { "neo-tree" },
-					ignore_focus = { "neo-tree", "lazy" },
-				},
-			})
-		end,
-	},
+  {
+    -- https://github.com/nvim-lualine/lualine.nvim
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("lualine").setup({
+        options = {
+          theme = "dracula",
+          globalstatus = false,
+          disabled_filetypes = { "neo-tree" },
+          ignore_focus = { "neo-tree", "lazy" },
+        },
+        sections = {
+          lualine_z = {
+            "location",
+            "searchcount",
+          },
+        },
+      })
+    end,
+  },
 }
