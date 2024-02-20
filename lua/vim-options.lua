f9aa43bbeb4e3ff97b853f4d763c5c782f4ea922
Darren <darrenchang951@gmail.com>
2024-02-20 10:18:33 +0800

    Show tailing spaces

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -7,8 +7,9 @@ vim.cmd("set statuscolumn=%r%s")
 vim.cmd("set signcolumn=auto:9")
 vim.cmd("set relativenumber number")
 vim.cmd("set cursorline")
+vim.cmd("set list")
+vim.cmd("set nowrap")
 vim.cmd("set cmdheight=0")
 vim.cmd("set mouse=a")
-vim.cmd("set nowrap")
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
