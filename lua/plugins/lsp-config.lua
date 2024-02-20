1b92e8752fedec55b68fadea47175624b3f8ca8d
Darren <darrenchang951@gmail.com>
2024-02-21 00:19:01 +0800

    Added autocomplete feature

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -2,6 +2,7 @@ return {
   {
     -- https://github.com/williamboman/mason.nvim
     "williamboman/mason.nvim",
+    lazy = false,
     config = function()
       require("mason").setup()
     end,
@@ -9,21 +10,36 @@ return {
   {
     -- https://github.com/williamboman/mason-lspconfig.nvim
     "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "marksman" },
+        ensure_installed = { "lua_ls", "tsserver", "marksman", "ruff_lsp" },
       })
     end,
   },
   {
     -- https://github.com/neovim/nvim-lspconfig
     "neovim/nvim-lspconfig",
+    lazy = false,
     config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
       local lspconfig = require("lspconfig")
       -- set up lsp servers
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({})
-      lspconfig.marksman.setup({})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.marksman.setup({
+        capabilities = capabilities,
+      })
+      lspconfig.ruff_lsp.setup({
+        capabilities = capabilities,
+      })
       -- set up keybindings
       vim.keymap.set("n", "<leader>b", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
