d89aa3cbc306fbe75d74c49007fb0bbe5a71c879
Darren <darrenchang951@gmail.com>
2024-02-26 02:13:39 +0000

    Added undotree keys to commander

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,11 +1,19 @@
 return {
   "mbbill/undotree",
-  cmd = "UndotreeToggle",
-  vim.cmd("nnoremap <leader>u :UndotreeToggle<CR>"),
-
-  -- persistent undo
-  vim.cmd("set undofile"),
-  vim.cmd("set undodir=~/.vim/undodir"),
-  vim.cmd("set undolevels=1000"),
-  vim.cmd("set undoreload=10000"),
+  -- cmd = "UndotreeToggle",
+  commander = {
+    {
+      cmd = "<CMD>UndotreeToggle<CR><CMD>UndotreeFocus<CR>",
+      desc = "Toggle undotree",
+      keys = { "n", "<leader>u" },
+      cat = "undotree",
+    },
+  },
+  config = function()
+    -- persistent undo
+    vim.cmd("set undofile")
+    vim.cmd("set undodir=~/.vim/undodir")
+    vim.cmd("set undolevels=1000")
+    vim.cmd("set undoreload=10000")
+  end
 }
