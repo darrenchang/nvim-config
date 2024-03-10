96a606780798d21094a75f7421a538376c0ee6de
darrenchang <darrenchang951@gmail.com>
2024-03-10 21:36:21 +0800

    Added smooth scroll feature

--- lua/plugins/eyecandy.lua
+++ lua/plugins/eyecandy.lua
@@ -19,4 +19,30 @@ return {
       })
     end,
   },
+  {
+    'karb94/neoscroll.nvim',
+    config = function()
+      require('neoscroll').setup({
+        mappings = {
+          '<C-u>',
+          '<C-d>',
+          '<C-b>',
+          '<C-f>',
+          '<C-y>',
+          '<C-e>',
+          'zt',
+          'zz',
+          'zb',
+        },
+        hide_cursor = true,
+        stop_eof = true,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing_function = "quadratic",
+        pre_hook = nil,
+        post_hook = nil, -- Function to run after the scrolling animation ends
+        performance_mode = false, -- Disable "Performance Mode" on all buffers.
+      })
+    end,
+  },
 }
