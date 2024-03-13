1347a8bd8e85c06cb8f95496b9ceff1f78864f2c
darrenchang <darrenchang951@gmail.com>
2024-03-13 19:18:56 +0800

    Move cursor to the middle when scrolled by page

--- lua/core/remaps.lua
+++ lua/core/remaps.lua
@@ -1,3 +1,5 @@
 -- to keep the cursor in the middle while moving and searching
 vim.keymap.set('n', 'n', 'nzz')
 vim.keymap.set('n', 'N', 'Nzz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
