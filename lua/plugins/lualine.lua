190b3b7a648432b640a50490b49c11335ea38439
Darren Chang <darrenchang951@gmail.com>
2024-07-06 16:47:51 +0800

    Added macro recording state to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,13 @@
+-- Format function for displaying macro recording status
+local function show_macro_recording()
+  local recording_register = vim.fn.reg_recording()
+  if recording_register == "" then
+    return ""
+  else
+    return "Recording @" .. recording_register
+  end
+end
+
 -- Returns a string with a list of attached LSP clients, including
 -- formatters and linters from null-ls, nvim-lint and formatter.nvim
 local function get_attached_clients()
@@ -117,6 +127,10 @@ return {
               'diagnostics',
               update_in_insert = true,
             },
+            {
+              "macro-recording",
+              fmt = show_macro_recording,
+            },
           },
           lualine_z = {
             attached_clients,
@@ -124,6 +138,35 @@ return {
             'searchcount',
           },
         },
+      })
+      -- Force refresh lualine as soon as entering recording state
+      vim.api.nvim_create_autocmd("RecordingEnter", {
+        callback = function()
+          require('lualine').refresh({
+            place = { "statusline" },
+          })
+        end,
+      })
+      -- Refresh lualine after leaving recording state
+      vim.api.nvim_create_autocmd("RecordingLeave", {
+        callback = function()
+          -- This is going to seem really weird!
+          -- Instead of just calling refresh we need to wait a moment because of the nature of
+          -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+          -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+          -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+          -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+          local timer = vim.loop.new_timer()
+          timer:start(
+            50,
+            0,
+            vim.schedule_wrap(function()
+              require('lualine').refresh({
+                place = { "statusline" },
+              })
+            end)
+          )
+        end,
       })
     end,
   },
