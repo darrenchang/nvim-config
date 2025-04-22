6b3b0646ea7c87c1fb1992eebd25d50b0bf0f1eb
darrenchang <darrenchang951@gmail.com>
2025-04-22 16:54:39 +0800

    Fix bug - better syntax highlight for .vue files
    - ensure vue, typescript, html, and css are installed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     config = function()
       local config = require('nvim-treesitter.configs')
       config.setup({
-        ensure_installed = { 'lua', 'javascript', 'python' },
+        ensure_installed = { 'lua', 'javascript', 'python', 'vue', 'typescript', 'html', 'css' },
         auto_instal = true,
         highlight = {
           enable = true,
