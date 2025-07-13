6ab238c92a3e53f08f357fcf13576c4b9a3c8531
darrenchang951 <darrenchang951@gmail.com>
2025-07-13 04:06:40 -0400

    Disable image

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,7 +6,7 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
-      '3rd/image.nvim',
+      -- '3rd/image.nvim',
     },
     commander = {
       {
