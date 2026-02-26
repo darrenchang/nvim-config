f4a4ed900768f99d182e6d46cb3fbd118b743bc4
Darren Chang <darrenchang951@gmail.com>
2026-02-25 20:25:56 -0800

    Fix: Update treesitter config syntax to v0.10.x

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,25 +2,22 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    config = function()
-      local config = require('nvim-treesitter.configs')
-      config.setup({
-        ensure_installed = {
-          'lua',
-          'javascript',
-          'python',
-          'vue',
-          'typescript',
-          'html',
-          'css',
-        },
-        auto_instal = true,
-        highlight = {
-          enable = true,
-          disable = { 'vimdoc' },
-        },
-        indent = { enable = true },
-      })
-    end,
+    opts = {
+      ensure_installed = {
+        'lua',
+        'javascript',
+        'python',
+        'vue',
+        'typescript',
+        'html',
+        'css',
+      },
+      auto_install = true,
+      highlight = {
+        enable = true,
+        disable = { 'vimdoc' },
+      },
+      indent = { enable = true },
+    },
   },
 }
