5feab97e92410d767b23d9bd0b7cb0906fbc5ed3
Darren Chang <darrenchang951@gmail.com>
2026-04-11 22:32:32 -0700

    Remove SmoothCursor

--- lua/plugins/eyecandy.lua
+++ lua/plugins/eyecandy.lua
@@ -10,23 +10,4 @@ return {
       hide_target_hack = false,
     },
   },
-  {
-    'gen740/SmoothCursor.nvim',
-    config = function()
-      require('smoothcursor').setup({
-        type = 'default',
-        speed = 25,
-        threshold = 3,
-        intervals = 40,
-        fancy = {
-          enable = true,
-          head = { cursor = '', texthl = 'SmoothCursor', linehl = nil },
-          body = {
-            { cursor = '', texthl = 'SmoothCursorOrange' },
-          },
-          tail = { cursor = nil, texthl = 'SmoothCursor' },
-        },
-      })
-    end,
-  },
 }
