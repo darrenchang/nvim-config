0916086755b76505cf91b18efc6654ee3b262261
Darren Chang <darrenchang951@gmail.com>
2026-04-11 23:44:24 -0700

    Refactor

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,7 +6,18 @@ return {
     name = 'catppuccin',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('catppuccin')
+      require('catppuccin').setup({
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15,
+        },
+        float = {
+          transparent = false, -- enable transparent floating windows
+          solid = false, -- use solid styling for floating windows, see |winborder|
+        },
+      })
+      vim.cmd.colorscheme('catppuccin-nvim')
     end,
   },
 }
