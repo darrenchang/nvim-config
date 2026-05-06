1e7d4e79379fa6dadb18e20ecec2e4e6fc2bb3cf
Darren Chang <darrenchang951@gmail.com>
2026-05-06 16:08:43 -0700

    Remove smear cursor - I use ghostty shader now

--- lua/plugins/eyecandy.lua
+++ lua/plugins/eyecandy.lua
@@ -1,13 +1,4 @@
 return {
   {
-    'sphamba/smear-cursor.nvim',
-    opts = {
-      legacy_computing_symbols_support = true,
-      cursor_color = '#00fff0',
-      stiffness = 0.8,
-      trailing_stiffness = 0.5,
-      distance_stop_animating = 0.5,
-      hide_target_hack = false,
-    },
   },
 }
